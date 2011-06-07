f0cfb8d41a8f5ee8a7e224ecdd60b24efa7d5219
Adam Rubin <adam@richminds.com>
2011-06-07 13:19:41 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocko::Application.config.secret_token = '2877e70ee3922d09eb0f1ce72c6ab7a7bb22682075fb4c677ec76f3f673e1b66ef56518753d72a5ef6aabadc53b5c5a24a14c29da78ffb21cc246ec46a1ff2f9'
