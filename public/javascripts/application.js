851390fd121e04a7a19b4c6ec0debd2e259af004
Adam Rubin <adam@richminds.com>
2011-06-10 14:32:12 -0400

    let's not overdo the client connections

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var client = null
+
+// $(document).ready(function() {
+// 	client = new Faye.Client(NODE_SERVER + '/faye');	
+// })
+
+
 function sendMessage(obj) {
-	// console.log( obj );
 	var message = $("#post_message").val();
-	var client = new Faye.Client(NODE_SERVER + '/faye');
 	client.publish('/foo', message);
 	$("#post_message").val("");
 	return false;
 }
-
-
-// $(document).ready(function() {
-// 	
-// });
