6dcd5cb89aeebb3230b2242ea5719d8bbce22519
Adam Rubin <adam@richminds.com>
2011-06-13 14:29:48 -0400

    cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,17 @@
 var client = null
+var subscription = null;
 
-// $(document).ready(function() {
-// 	client = new Faye.Client(NODE_SERVER + '/faye');	
-// })
-
+$(document).ready(function() {
+	client = new Faye.Client(NODE_SERVER + '/faye');	
+	subscription = client.subscribe('/foo', function(message) {
+		$("#messages ul").prepend("<li>" + message + "</li>");
+	});
+})
 
 function sendMessage(obj) {
 	var message = $("#post_message").val();
 	client.publish('/foo', message);
 	$("#post_message").val("");
+
 	return false;
 }
